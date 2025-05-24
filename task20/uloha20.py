13eb899eb4e9f916b21fb3cc52a7d7c1507f14bf
Fireworks <fireworks1602@gmail.com>
2025-05-24 09:12:21 +0200

    mistakes in task 20

--- task20/uloha20.py
+++ task20/uloha20.py
@@ -1,49 +1,44 @@
- 
-with open("./task20/knihy.txt", "r")as file:
-    books = []
-    dates = []
-    for idx, data in enumerate(file.readlines()):
-        if idx%2 == 0: # first line
-            books.append(data.strip())
-        else:          # second line
-            dates.append(data.strip())
-
-# if dates is odd
-for idx, temp in enumerate(dates):
-    notification = temp.split()
-    if len(notification)%2!=0:
-        print(books[idx])
-
-max_length = max(len(line.split()) for line in dates)
-longest_lines = "\n".join([books[idx] for idx,line in enumerate(dates) if len(line.split()) == max_length])
-print(longest_lines)
-
-# date time
 from datetime import datetime
 
-# Funkcia na výpočet počtu dní medzi dvoma dátumami
-def calculate_days(date1, date2):
-    date_format = "%d%m"
-    d1 = datetime.strptime(date1, date_format)
-    d2 = datetime.strptime(date2, date_format)
-    return (d2 - d1).days
+with open("./task20/knihy.txt", "r") as f:
+    data = f.readlines()
+    books = [line.strip() for pos ,line in enumerate(data) if pos % 2 == 0]
+    dates = [line.strip().split() for pos ,line in enumerate(data) if pos % 2 != 0]
+    
 
-# Pre každú knihu vypočítame celkovú dobu vypožičania
-today = "1506"
-books_with_duration = []
-for idx,book in enumerate(dates):
-    dates = book.split()
-    total_days = 0
-    for i in range(0, len(dates), 2):
-        if i + 1 < len(dates):  # Zabezpečíme, že máme pár dátumov
-            total_days += calculate_days(dates[i], dates[i + 1])
-        else:
-            total_days += calculate_days(dates[i], today)
-    books_with_duration.append((books[idx], total_days))
+def date_calculator(date1, date2):
+    date1 = datetime.strptime(date1, "%d%m")
+    date2 = datetime.strptime(date2, "%d%m")
+    delta = date2 - date1
+    return delta.days
 
-# Zoradenie kníh podľa celkovej doby vypožičania (od najkratšej po najdlhšiu)
-sorted_books = sorted(books_with_duration, key=lambda x: x[1])
+# uloha a + c
+today = "1508" 
+result  = []
+for pos, line in enumerate(dates):
+    if len(line) % 2 != 0: 
+        if date_calculator(line[-1], today) > 30: 
+            print(f"{books[pos]}")
+        result.append([date_calculator(line[-1], today), books[pos]]) 
+    else: 
+        result.append([date_calculator(line[-2], line[-1]), books[pos]])
 
-# Výpis zoradených kníh
-for book, duration in sorted_books:
-    print(f"{book} (Celková doba vypožičania: {duration} dní)")
+print(result)
+result.sort()
+print(result)
+
+# uloha b
+most_borrowed = 0
+for line in dates:
+    count = 0
+    for date in range(0, len(line), 2): 
+        count += 1 
+    if count > most_borrowed:
+        most_borrowed = count
+
+for pos ,line in enumerate(dates):
+    count = 0
+    for date in range(0, len(line), 2):
+        count += 1 
+    if count == most_borrowed: 
+        print(f"{books[pos]}-most borrowed")
